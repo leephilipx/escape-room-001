2239814f00b7b5c31a8564aa9376dc71569c1e3d
Philip Lee <leephiliphy@gmail.com>
2025-12-26 00:14:59 +0800

    update deployment workflow

--- hourglass-realm/frontend/eslint.config.js
+++ hourglass-realm/frontend/eslint.config.js
@@ -8,7 +8,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    base: '/apps/journal-vault/',
+    base: '/apps/hourglass-realm/',
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
